ec1bb8dd8664df11560a104b0a00bc44e09a2383
Draggo08 <draggo08@mail.ru>
2023-11-22 14:51:49 +0300

    Repetitor Don

---
+++ src/lab2/vigenere/test_vigener.py
@@ -0,0 +1,20 @@
+import unittest
+from vigenere import encrypt_vigenere, decrypt_vigenere
+
+class TestVigenereCipher(unittest.TestCase):
+    def test_encrypt_vigenere(self):
+        # Проверка шифрования
+        self.assertEqual(encrypt_vigenere("ATTACKATDAWN", "LEMON"), "LXFOPVEFRNHR")
+        self.assertEqual(encrypt_vigenere("PYTHON", "A"), "PYTHON")
+        self.assertEqual(encrypt_vigenere("python", "a"), "python")
+        self.assertEqual(encrypt_vigenere("", "KEY"), "")
+
+    def test_decrypt_vigenere(self):
+        # Проверка дешифрования
+        self.assertEqual(decrypt_vigenere("LXFOPVEFRNHR", "LEMON"), "ATTACKATDAWN")
+        self.assertEqual(decrypt_vigenere("PYTHON", "A"), "PYTHON")
+        self.assertEqual(decrypt_vigenere("python", "a"), "python")
+        self.assertEqual(decrypt_vigenere("", "KEY"), "")
+
+if name == "__main__":
+    unittest.main()
